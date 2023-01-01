f0b0b13673bdacf27cd40d4d3460a72de13f3562
Johnny <johnny.martens@protonmail.com>
2023-01-01 17:13:50 -0500

    init commit

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var Taylorlabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func change(_ sender: Any) {
+        imageView.image = UIImage(named: "tswift2")
+    }
+    
 }
 
